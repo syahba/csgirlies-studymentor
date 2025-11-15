8dbb32e547e82fe1c815a44ddafd15006db266ed
syahba <ksyaahba@gmail.com>
2025-11-15 20:19:42 +0700

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>} />
+        <Route path="/register" element={<RegisterPage></RegisterPage>} />
+        <Route path="/session/setup" element={<SetUpPage></SetUpPage>} />
+        <Route path="/session/:id" element={<StudyPage></StudyPage>} />
+        <Route path="/session/:id/quiz/:quizId" element={<QuizPage></QuizPage>} />
+        <Route path="/error" element={<ErrorPage></ErrorPage>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
