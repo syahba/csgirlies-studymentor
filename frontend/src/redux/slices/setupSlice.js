4f5bf012ba8dcaec3a55259534756c7a50b730d6
syahba <ksyaahba@gmail.com>
2025-11-16 17:49:16 +0700

    setup slice

--- frontend/src/redux/slices/setupSlice.js
+++ frontend/src/redux/slices/setupSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   room_name: "",       // LiveKit room name
@@ -32,5 +33,75 @@ const sessionSlice = createSlice({
 
 export const { setRoomName, setInput, setMentorId, addFile, removeFile, setFilePaths, clearSession } =
   sessionSlice.actions;
+
+// room name -> topic (input) + datetime
+export const generateRoomName = (topic) => {
+  return (dispatch) => {
+    const datetime = new Date().toISOString().replace(/[-:.]/g, "");
+    const roomName = `${topic.replace(/\s+/g, "_")}_${datetime}`;
+    dispatch(setRoomName(roomName));
+  }
+}
+
+// select topic -> when user inputs a study topic
+export const selectTopic = (topic) => {
+  return (dispatch) => {
+    dispatch(setInput(topic));
+  }
+}
+
+// add file -> when user selects a document to upload
+export const addFiles = (documents) => {
+  return (dispatch) => {
+    documents.forEach((doc) => dispatch(addFile(doc)));
+  };
+};
+
+// remove file by index -> when user removes a selected document
+export const removeFiles = (index) => {
+  return (dispatch) => {
+    dispatch(removeFile(index));
+  };
+};
+
+// upload -> when user uploading a document to get the file path
+export const uploadFiles = (formData, roomName) => {
+  return async (dispatch) => {
+    const res = await axios.post(
+      `http://localhost:3001/api/v1/file/upload?room_name=${roomName}`,
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+
+    const files = res?.data?.files || [];
+
+    dispatch(setFilePaths(files.map((f) => f.path)));
+  };
+};
+
+// add session -> payload {userId, room_name, file_paths }
+export const addSession = (payload) => {
+  return async () => {
+    await axios.post(
+      "http://localhost:3001/api/v1/session/add",
+      payload,
+      { headers: { "Content-Type": "application/json" } }
+    );
+  };
+};
+
+// select mentor -> when user selects a mentor
+export const selectMentor = (mentorId) => {
+  return (dispatch) => {
+    dispatch(setMentorId(mentorId));
+  };
+};
+
+// clear setup session -> when user starts a new session
+export const clearSetupSession = () => {
+  return (dispatch) => {
+    dispatch(clearSession());
+  };
+};
 
 export default sessionSlice.reducer;
